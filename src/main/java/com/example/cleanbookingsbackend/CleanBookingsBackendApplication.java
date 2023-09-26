f126dcb393174ddff39aa3375181616efa2588d6
SebastianKärner88 <sebastiankarner@live.se>
2023-09-26 14:41:37 +0200

    Initial commit

---
+++ src/main/java/com/example/cleanbookingsbackend/CleanBookingsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cleanbookingsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanBookingsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanBookingsBackendApplication.class, args);
+    }
+
+}
