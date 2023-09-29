a7a86efd4006828269a940ba14efab1692113840
gdrci <gdrakos.ce@gmail.com>
2023-09-29 18:48:53 +0200

    /booked-cleanings/{customerId} endpoint improvements

--- src/main/java/com/example/cleanbookingsbackend/controller/JobController.java
+++ src/main/java/com/example/cleanbookingsbackend/controller/JobController.java
@@ -23,6 +23,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/job")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class JobController {
     private final JobService jobService;
 
@@ -63,6 +64,7 @@ public class JobController {
 
     @GetMapping("/booked-cleanings/{customerId}")
     public ResponseEntity<List<JobDto>> getBookedCleanings(@PathVariable String customerId) {
+        System.out.println("Received customerId: " + customerId);
         List<JobEntity> jobs = jobService.getBookedCleaningsForCustomer(customerId);
 
         // Convert JobEntity objects to JobDto objects
@@ -83,4 +85,6 @@ public class JobController {
 
         return jobDto;
     }
+
+
 }
