cf9850325479b1a222b7512bb6a40a9afdc10f34
gdrci <gdrakos.ce@gmail.com>
2023-09-29 16:01:05 +0200

    Finished /booked-cleanings/{customerId} endpoint

--- src/main/java/com/example/cleanbookingsbackend/service/JobService.java
+++ src/main/java/com/example/cleanbookingsbackend/service/JobService.java
@@ -25,7 +25,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -34,6 +33,7 @@ public class JobService {
     private final CustomerRepository customerRepository;
     private final EmployeeRepository employeeRepository;
     private final JavaMailSender mailSender;
+
 
     public CreateJobResponse createJobRequest(CreateJobRequest request)
             throws IllegalArgumentException, CustomerNotFoundException, ParseException {
@@ -200,18 +200,10 @@ public class JobService {
                 .build();
     }
 
-    public List<JobEntity> getBookedCleaningsForCustomer(String customerId) throws CustomerNotFoundException {
-        CustomerEntity customer = validateCustomerId2(customerId);
-        return jobRepository.findByCustomerAndStatusNot(customer, JobStatus.CLOSED);
-    }
 
-    private CustomerEntity validateCustomerId2(String id) throws CustomerNotFoundException {
-        Optional<CustomerEntity> customer = customerRepository.findById(id);
-        if (customer.isPresent()) {
-            return customer.get();
-        } else {
-            throw new CustomerNotFoundException("Customer not found with ID: " + id);
-        }
+    public List<JobEntity> getBookedCleaningsForCustomer(String customerId) {
+
+        return jobRepository.findByCustomer_Id(customerId);
     }
 
 }
