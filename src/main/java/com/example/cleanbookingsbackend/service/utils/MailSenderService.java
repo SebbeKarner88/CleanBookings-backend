5a40963417c92fd6116484a5e7b8149ae49f6fc8
SebastianKärner88 <sebastiankarner@live.se>
2023-10-04 10:55:21 +0200

    CB-74 final changes to dateformat and linechange for better visuals.

--- src/main/java/com/example/cleanbookingsbackend/service/utils/MailSenderService.java
+++ src/main/java/com/example/cleanbookingsbackend/service/utils/MailSenderService.java
@@ -23,7 +23,7 @@ public class MailSenderService {
         msg.setFrom("order.cleanbookings@gmail.com");
         msg.setTo(requestedJob.getCustomer().getEmailAddress());
         msg.setSubject("Din bokningsbekräftelse");
-        msg.setText("Hej " + requestedJob.getCustomer().getFirstName() + "! Din bokning av " + requestedJob.getType() + " på " + requestedJob.getBookedDate() + " har bekräftats./n/n" +
+        msg.setText("Hej " + requestedJob.getCustomer().getFirstName() + "! Din bokning av " + requestedJob.getType() + " på " + requestedJob.getBookedDate() + " har bekräftats.\n\n" +
                 "StädaFint AB");
         try {
             mailSender.send(msg);
@@ -37,8 +37,8 @@ public class MailSenderService {
         msg.setFrom("order.cleanbookings@gmail.com");
         msg.setTo(requestedCancel.getCustomer().getEmailAddress());
         msg.setSubject("Avbokad städning");
-        msg.setText("Hej " + requestedCancel.getCustomer().getFirstName() + "! Er bokning av " + requestedCancel.getType() + " på " + requestedCancel.getBookedDate() + " är nu avbokad. /n" +
-                "Varmt välkommen åter!/n/n" +
+        msg.setText("Hej " + requestedCancel.getCustomer().getFirstName() + "! Er bokning av " + requestedCancel.getType() + " på " + requestedCancel.getBookedDate() + " är nu avbokad. \n\n" +
+                "Varmt välkommen åter!\n" +
                 "StädaFint AB");
         try {
             mailSender.send(msg);
@@ -53,8 +53,8 @@ public class MailSenderService {
         msg.setFrom("order.cleanbookings@gmail.com");
         msg.setTo(cleaner.getEmailAddress());
         msg.setSubject("Nytt städjobb för " + cleaner.getFirstName() + "!");
-        msg.setText("Hej " + cleaner.getFirstName() + "! /n/nDu har fått ett nytt städjobb inbokat: "
-                + job.getBookedDate() + ", " + job.getType() + "Meddelande: " + job.getMessage() + "/n/nFör mer information logga in på CleanBookings.");
+        msg.setText("Hej " + cleaner.getFirstName() + "! \n\nDu har fått ett nytt städjobb inbokat: "
+                + job.getBookedDate() + ", " + job.getType() + "Meddelande: " + job.getMessage() + "\n\nFör mer information logga in på CleanBookings.");
         try {
             mailSender.send(msg);
         } catch (MailException exception) {
@@ -82,10 +82,9 @@ public class MailSenderService {
         msg.setFrom("order.cleanbookings@gmail.com");
         msg.setTo(job.getCustomer().getEmailAddress());
         msg.setSubject("Tack för förtroendet!");
-        msg.setText("Hej " + job.getCustomer().getFirstName() + "! /n/nNi har nu godkänt städningen vi har utfört hos er." +
-                "/n/nVi tackar så mycket för ert förtroende och hoppas att ni väljer oss igen nästa gång ni behöver hjälp med städ. " +
-                "{" +
-                "/n/n Betalning kommer ske enligt överenskommelse vid bokning./n/n" +
+        msg.setText("Hej " + job.getCustomer().getFirstName() + "! \n\nNi har nu godkänt städningen vi har utfört hos er." +
+                "\n\nVi tackar så mycket för ert förtroende och hoppas att ni väljer oss igen nästa gång ni behöver hjälp med städ. " +
+                "\n\nBetalning kommer ske enligt överenskommelse vid bokning.\n\n" +
                 "StädaFint AB");
         try {
             mailSender.send(msg);
@@ -99,8 +98,8 @@ public class MailSenderService {
         msg.setFrom("order.cleanbookings@gmail.com");
         msg.setTo(job.getCustomer().getEmailAddress());
         msg.setSubject("Tack för din feedback!");
-        msg.setText("Hej " + job.getCustomer().getFirstName() + "! /n/nHär kommer en bekräftelse på att vi mottagit ert meddelande." +
-                "/n/nVi ber om ursäkt att vårat jobb inte levde upp till era förväntningar. /nVi har meddelat våra städare och åtgärd kommer att utföras inom kort. /n/n" +
+        msg.setText("Hej " + job.getCustomer().getFirstName() + "! \n\nHär kommer en bekräftelse på att vi mottagit ert meddelande." +
+                "\n\nVi ber om ursäkt att vårat jobb inte levde upp till era förväntningar. \nVi har meddelat våra städare och åtgärd kommer att utföras inom kort. \n\n" +
                 "StädaFint AB");
         try {
             mailSender.send(msg);
