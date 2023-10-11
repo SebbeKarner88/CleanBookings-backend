59f2cd298941b2ce8d6249bba6eeb2b769a6cebb
SebastianKärner88 <sebastiankarner@live.se>
2023-10-11 13:31:51 +0200

    Refactor on Mailservice, broke out logic to be re-used

--- src/main/java/com/example/cleanbookingsbackend/service/utils/MailSenderService.java
+++ src/main/java/com/example/cleanbookingsbackend/service/utils/MailSenderService.java
@@ -9,6 +9,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
@@ -18,14 +19,12 @@ public class MailSenderService {
     private final static String CLEAN_BOOKINGS = "order.cleanbookings@gmail.com";
     private final static String EMAIL_NOT_SENT = "Email couldn't be sent: ";
 
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-
     public void sendEmailConfirmationBookedJob(JobEntity requestedJob) {
         msg.setFrom(CLEAN_BOOKINGS);
         msg.setTo(getCustomerEmailAdress(requestedJob));
         msg.setSubject("Din bokningsförfrågan.");
         msg.setText("Hej " + getCustomerName(requestedJob) + "! Er bokning av " + requestedJob.getType() + " den "
-                + getFormattedDate(requestedJob) + " har tagits emot av oss.\n\nNi kommer att få en bekräftelse på " +
+                + getFormattedDateAndTime(requestedJob.getBookedDate()) + " har tagits emot av oss.\n\nNi kommer att få en bekräftelse på " +
                 "bokningen när vi bokat in en städare på ert jobb.\n\n" + "StädaFint AB");
         try {
             mailSender.send(msg);
@@ -39,7 +38,7 @@ public class MailSenderService {
         msg.setTo(getCustomerEmailAdress(requestedCancel));
         msg.setSubject("Avbokad städning");
         msg.setText("Hej " + getCustomerName(requestedCancel) + "! Er bokning av " + requestedCancel.getType() +
-                " den " + getFormattedDate(requestedCancel) + " är nu avbokad. \n\n" +
+                " den " + getFormattedDateAndTime(requestedCancel.getBookedDate()) + " är nu avbokad. \n\n" +
                 "Varmt välkommen åter!\n" +
                 "StädaFint AB");
         try {
@@ -55,7 +54,7 @@ public class MailSenderService {
         msg.setTo(cleaner.getEmailAddress());
         msg.setSubject("Nytt städjobb för " + cleaner.getFirstName() + "!");
         msg.setText("Hej " + cleaner.getFirstName() + "! \n\nDu har fått ett nytt städjobb inbokat: "
-                + getFormattedDate(job) + ", " + job.getType() + "Meddelande: " + job.getMessage() +
+                + getFormattedDateAndTime(job.getBookedDate()) + ", " + job.getType() + "Meddelande: " + job.getMessage() +
                 "\n\nFör mer information logga in på CleanBookings.");
         try {
             mailSender.send(msg);
@@ -139,7 +138,7 @@ public class MailSenderService {
         msg.setSubject("Faktura på utförd städning");
         msg.setText("Faktura StädaFint AB                              Fakturanummer: " + job.getPayment().getId() + "\n\n\n\n" +
                 "Betalvillkor: 30 dagar netto                      Betalsätt: Faktura\n" +
-                "Fakturadatum: " + sdf.format(job.getPayment().getIssueDate()) + "                          Förfallodag: " + sdf.format(job.getPayment().getDueDate()) + "\n\n" +
+                "Fakturadatum: " + getFormattedDate(job.getPayment().getIssueDate()) + "                          Förfallodag: " + getFormattedDate(job.getPayment().getDueDate()) + "\n\n" +
                 "____________________________________________________________________\n" +
                 job.getType() + "                Antal: 1                 Pris: " + job.getPayment().getPrice() + "kr ink. moms.\n" +
                 "____________________________________________________________________\n" +
@@ -176,7 +175,11 @@ public class MailSenderService {
         return requestedCancel.getCustomer().getFirstName();
     }
 
-    private static String getFormattedDate(JobEntity requestedJob) {
-        return new SimpleDateFormat("yyyy-MM-dd hh:mm").format(requestedJob.getBookedDate());
+    private static String getFormattedDateAndTime(Date date) {
+        return new SimpleDateFormat("yyyy-MM-dd hh:mm").format(date);
+    }
+
+    private static String getFormattedDate(Date date) {
+        return new SimpleDateFormat("yyyy-MM-dd").format(date);
     }
 }
