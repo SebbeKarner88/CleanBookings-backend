50d7f302d27c1982b963062a1b983e8c4c833995
SebastianKärner88 <sebastiankarner@live.se>
2023-10-13 10:29:13 +0200

    Made soc-sec number final on PrivateCustomerEntity

--- src/main/java/com/example/cleanbookingsbackend/model/PrivateCustomerEntity.java
+++ src/main/java/com/example/cleanbookingsbackend/model/PrivateCustomerEntity.java
@@ -8,29 +8,27 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
-
 @Entity
 @Data
 @Builder
-@NoArgsConstructor
+@NoArgsConstructor(force = true)
 @AllArgsConstructor
 @Table(name = "private_customer", uniqueConstraints = @UniqueConstraint(name = "username_private_customer_unique", columnNames = "email_address"))
 public class PrivateCustomerEntity {
 
     @Id
-    @GeneratedValue(
-            strategy = GenerationType.UUID
-    )
+    @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "id", columnDefinition = "text")
     private String id;
 
     @Column(name = "first_name", columnDefinition = "text")
     private String firstName;
+
     @Column(name = "last_name", columnDefinition = "text")
     private String lastName;
 
     @Column(name = "social_security_number", columnDefinition = "text")
-    private String personNumber;
+    private final String personNumber;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "customer_type")
@@ -38,19 +36,26 @@ public class PrivateCustomerEntity {
 
     @Column(name = "street_address", columnDefinition = "text")
     private String streetAddress;
+
     @Column(name = "postal_code", columnDefinition = "varchar(5)")
     private Integer postalCode;
+
     @Column(name = "city", columnDefinition = "text")
     private String city;
+
     @Column(name = "phone_number", columnDefinition = "text")
     private String phoneNumber;
 
     @Column(name = "email_address", columnDefinition = "text")
     private String emailAddress;
+
     @Column(name = "password", columnDefinition = "text", nullable = false)
     private String password;
 
     @OneToMany(mappedBy = "customer")
     private List<JobEntity> jobs;
 
+    public PrivateCustomerEntity(String personNumber) {
+        this.personNumber = personNumber;
+    }
 }
