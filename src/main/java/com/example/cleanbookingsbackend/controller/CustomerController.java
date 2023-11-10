d784a9e3d8723dc33b6b7f86272bb64709fd6d90
@jimmy.brissman <jimmy.brissman@protonmail.com>
2023-11-10 13:46:32 +0100

    REFACTOR: Cancel job request as customer
    Rewrote the method to extract the customerId from the header instead of requiring the client to send this in the body as well.
    
    Deleted code that no longer is needed due to the role-based authentication with the JWT generated from Keycloak. Only a user with the role of customer can access this end-point.

--- src/main/java/com/example/cleanbookingsbackend/controller/CustomerController.java
+++ src/main/java/com/example/cleanbookingsbackend/controller/CustomerController.java
@@ -6,6 +6,7 @@ import com.example.cleanbookingsbackend.service.CustomerService;
 import jakarta.security.auth.message.AuthException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
--- src/main/java/com/example/cleanbookingsbackend/controller/JobController.java
+++ src/main/java/com/example/cleanbookingsbackend/controller/JobController.java
@@ -6,6 +6,7 @@ import com.example.cleanbookingsbackend.enums.Role;
 import com.example.cleanbookingsbackend.exception.*;
 import com.example.cleanbookingsbackend.model.JobEntity;
 import com.example.cleanbookingsbackend.service.JobService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -106,15 +107,18 @@ public class JobController {
 
     @PreAuthorize("hasRole('client_customer')")
     @DeleteMapping
-    public ResponseEntity<?> cancelJobRequest(@RequestBody JobUserRequest request) {
+    public ResponseEntity<?> cancelJobRequest(
+            @RequestParam String jobId,
+            HttpServletRequest request) {
         try {
-            return ResponseEntity.status(HttpStatus.OK).body(jobService.cancelJobRequest(request));
+            jobService.cancelJobRequest(jobId, request);
+            return ResponseEntity.noContent().build();
         } catch (IllegalArgumentException | UnauthorizedCallException exception) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
         } catch (NotFoundException | JobNotFoundException exception) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
         } catch (Exception e) {
-            return ResponseEntity.internalServerError().body("Something went wrong, and I don't know why...");
+            return ResponseEntity.internalServerError().body(e.getMessage());
         }
     }
 
